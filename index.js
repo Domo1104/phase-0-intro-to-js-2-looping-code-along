f7d80cfc6990c2799252c09ea60334651baf6d4f
ChristopherOu <ChrisOu@yahoo.com>
2022-07-19 02:08:38 -0400

    commit tree 57b3b74bf03a1e445b8c51e7914b4350740717e2

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let finishedCards = []
+    for (let i = 0; i < name.length; i++) {
+        finishedCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    } return finishedCards
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function countDown(n) {
+    let i = n;
+    while (i >= 0)
+    console.log(i--)
+}
